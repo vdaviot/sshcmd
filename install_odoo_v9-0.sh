14f409d63edc275ba631eeb468a93c4c34983b77
vdaviot <vdaviot@student.42.fr>
2017-07-20 03:47:58 -0700

    Simpler install script

---
+++ install_odoo_v9-0.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt-get install postgresql
+sudo su
+wget -O - https://nightly.odoo.com/odoo.key | apt-key add -
+echo "deb http://nightly.odoo.com/9.0/nightly/deb/ ./" >> /etc/apt/sources.list
+apt-get update && apt-get install odoo
