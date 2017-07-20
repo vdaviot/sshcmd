e8fc918fd8cac4f28b01462bfcbbb249d66fb9ed
vdaviot <vdaviot@student.42.fr>
2017-07-20 05:21:47 -0700

    added alliases

--- install_odoo_v9-0.sh
+++ install_odoo_v9-0.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-sudo apt-get install postgresql
+sudo apt-get install postgresql zsh
+zsh
 sudo su
 wget -O - https://nightly.odoo.com/odoo.key | apt-key add -
 echo "deb http://nightly.odoo.com/9.0/nightly/deb/ ./" >> /etc/apt/sources.list
 apt-get update && apt-get install odoo
+
+echo "alias odoo=\"odoo.py start " >> ~/.zshrc
+echo "alias restart=\"odoo.py restart" >> ~/.zshrc
+echo "alias scaffold=\"odoo.py scaffold" >> ~/.zshrc
