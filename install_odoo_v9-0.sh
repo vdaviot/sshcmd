c97e34144e81ddc5bf9e2c178f63002eb9d821a5
vdaviot <vdaviot@student.42.fr>
2017-07-20 05:41:24 -0700

    lol

--- install_odoo_v9-0.sh
+++ install_odoo_v9-0.sh
@@ -7,6 +7,10 @@ wget -O - https://nightly.odoo.com/odoo.key | apt-key add -
 echo "deb http://nightly.odoo.com/9.0/nightly/deb/ ./" >> /etc/apt/sources.list
 apt-get update && apt-get install odoo
 
+export ODOO_ADDON_PATH="/usr/lib/python2.7/dist-packages/openerp/addons/"
+
 echo "alias odoo=\"odoo.py start " >> ~/.zshrc
 echo "alias restart=\"odoo.py restart" >> ~/.zshrc
-echo "alias scaffold=\"odoo.py scaffold" >> ~/.zshrc
+echo "alias scaffold=\"odoo.py scaffold " >> ~/.zshrc
+
+echo "alias addon=\"scaffold $ODOO_ADDON_PATH"
